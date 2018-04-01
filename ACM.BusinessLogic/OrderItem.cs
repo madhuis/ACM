d438db7ef35008b5ea775c4231a627839373314b
Ishan Madhu <imadhu2991@gmail.com>
2018-04-01 16:05:48 -0500

    Added OrderItem

---
+++ ACM.BusinessLogic/OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BusinessLogic
+{
+    class OrderItem
+    {
+
+        public OrderItem() { }
+
+        public OrderItem(int orderItem)
+        {
+            this.OrderItemID = orderItem;
+        }
+
+        public int OrderItemID { get; private set; }
+
+        public int OrderQuantity { get; set; }
+
+        public int ProductID { get; set; }
+
+        public decimal? PurchasePrice { get; set; }
+
+        // Retrieve the Order Item
+        public OrderItem Retrieve()
+        {
+            return new OrderItem();
+        }
+
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = true;
+
+            if (OrderQuantity <= 0)
+                isValid = false;
+
+            if (ProductID <= 0)
+                isValid = false;
+
+            if (PurchasePrice == null)
+                isValid = false;
+
+            return isValid;
+        }
+
+    }
+}
